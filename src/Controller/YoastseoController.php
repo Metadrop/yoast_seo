5d8b8daf5ecf6864999259ee05157a3dd5944ebe
Kevin Muller <mynameisgnu@gmail.com>
2015-11-30 15:57:58 +0530

    initial commit

---
+++ src/Controller/YoastseoController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\yoast_seo\Controller\YoastseoController.
+ */
+
+namespace Drupal\yoast_seo\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class YoastseoController extends ControllerBase {
+	public function content() {
+		return array(
+			'#type' => 'markup',
+			'#markup' => $this->t('Hello, World!'),
+		);
+	}
+}
