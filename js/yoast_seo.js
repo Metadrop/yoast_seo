9b66c0e00f0b96d8a0dce598372f5949182f68fe
Cedric Alfonsi <cedricalfonsi@gmail.com>
2015-12-21 12:18:23 +0100

    Start the implmentation of the getData function. The YoastSeo callback function to retrieve the meaningfull data

--- js/yoast_seo.js
+++ js/yoast_seo.js
@@ -11,15 +11,64 @@
 (function ($, Drupal) {
 
   Drupal.behaviors.yoastSeo = {
-    attach: function (context) {
+    attach: function (context, settings) {
       var $context = $(context),
         formItems = {};
 
+      var fields = {
+        keyword: 'edit-field-yoast-seo-0-yoast-seo-focus-keyword', // or 'edit-field-meta-tags-0-basic-keywords',
+        meta: 'edit-field-meta-tags-0-basic-description',
+        text: 'edit-body-0-value',
+        pageTitle: 'edit-field-meta-tags-0-basic-title',
+        title: 'edit-field-meta-tags-0-basic-title'
+      };
+
+      YoastSeoData = {
+        /**
+         * This should return an object with the given properties
+         *
+         * @callback YoastSEO.App~getData
+         * @returns {Object} data
+         * @returns {String} data.keyword The keyword that should be used
+         * @returns {String} data.meta
+         * @returns {String} data.text The text to analyze
+         * @returns {String} data.pageTitle The text in the HTML title tag
+         * @returns {String} data.title The title to analyze
+         * @returns {String} data.url The URL for the given page
+         * @returns {String} data.excerpt Excerpt for the pages
+         */
+        getData: function () {
+          var data = {
+            keyword: '',
+            meta: '',
+            text: '',
+            pageTitle: '',
+            title: '',
+            url: '',
+            excerpt: ''
+          };
+
+          for (var fieldName in data) {
+            var formItemView = Drupal.YoastSeoForm._formItemViews[fields[fieldName]];
+            if (typeof formItemView !== 'undefined') {
+              data[fieldName] = formItemView.value();
+            }
+          }
+
+          return data;
+        }
+      };
+
       // Construct the form
-      $('.js-form-item', $('#node-page-edit-form')).each(function () {
-        YoastSeoForm.getFormItemView(this);
+      $('.js-form-item', $('#node-page-form')).each(function () {
+        Drupal.YoastSeoForm.getFormItemView(this);
       });
 
+      $(window).on('yoast_seo-form_item-changed', function() {
+        console.log(YoastSeoData.getData());
+      });
+
+      console.log(YoastSeoData.getData());
     }
   };
 
