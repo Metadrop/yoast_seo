2ab5058556e06c667e92d22a7e6fd5b5e8516f35
Alexander Varwijk <alexander@goalgorilla.com>
2017-09-28 10:45:24 +0200

    Refactor Entity preview generation into a service

--- src/Controller/EntityPreviewController.php
+++ src/Controller/EntityPreviewController.php
@@ -2,30 +2,10 @@
 
 namespace Drupal\yoast_seo\Controller;
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\Common\Annotations\SimpleAnnotationReader;
-use Doctrine\Common\Reflection\StaticReflectionParser;
-use Drupal\Component\Annotation\Reflection\MockFileFinder;
-use Drupal\Core\Config\Entity\ConfigEntityBase;
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Entity\ContentEntityBase;
-use Drupal\Core\Entity\Entity;
-use Drupal\Core\Entity\EntityFormBuilder;
-use Drupal\Core\Form\EnforcedResponseException;
-use Drupal\Core\Form\FormAjaxException;
-use Drupal\Core\Form\FormState;
-use Drupal\Core\Routing\RouteBuilder;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Url;
-use Drupal\node\Entity\Node;
-use Drupal\node\Form\NodePreviewForm;
-use Drupal\user\Entity\User;
-use Drupal\user\EntityOwnerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Zend\Diactoros\Response\TextResponse;
 
 /**
  * Provides methods for interacting with entity previews.
@@ -74,67 +54,11 @@ class EntityPreviewController extends ControllerBase {
       throw new BadRequestHttpException("Missing form_id");
     }
 
-    /** @var \Symfony\Cmf\Component\Routing\ChainRouter $router */
-    $router = \Drupal::service('router.no_access_checks');
+    $entity_previewer = \Drupal::service('yoast_seo.entity_previewer');
 
-    $form_request = Request::create($preview_context['action'], $preview_context['method'], $form_data);
+    $entity = $entity_previewer->entityFromFormSubmission($preview_context['action'], $preview_context['method'], $form_data);
 
-    // Push our form request on the stack so it's used by the form builder.
-    \Drupal::requestStack()->push($form_request);
-
-    try {
-      $match = $router->matchRequest($form_request);
-    }
-    catch (\Exception $e) {
-      throw new NotFoundHttpException('Could not find the entity route.');
-    }
-
-    $operation = NULL;
-
-    // Find the first loaded entity.
-    foreach ($match as $val) {
-      // Existing entities will provide is with an entity of ContentEntityBase.
-      if ($val instanceof ContentEntityBase || is_subclass_of($val, "ContentEntityBase")) {
-        $temp_entity = $val;
-
-        $operation = explode('.', $match['_entity_form'])[1];
-        break;
-      }
-      // Entity creation pages provide us a parent ConfigEntityBase inheritor to
-      // help us determine the entity type and bundle.
-      elseif ($val instanceof ConfigEntityBase || is_subclass_of($val, "ConfigEntityBase")) {
-        $config_entity_type = $val->getEntityType();
-        $type = $config_entity_type->getBundleOf();
-
-        $operation = explode('.', $match['_route'])[1];
-
-        $storage = $this->entityTypeManager()->getStorage($type);
-
-        // Set the bundle name where needed.
-        $type_key = $storage->getEntityType()->get('entity_keys')['bundle'];
-
-        // Create a temporary entity so we can load the form.
-        $temp_entity = $storage->create([$type_key => $val->id()]);
-
-        break;
-      }
-    }
-
-    if (empty($temp_entity)) {
-      throw new BadRequestHttpException('Could not construct entity from form data');
-    }
-
-    $entity = $this->getUpdatedEntity($temp_entity, $operation);
-
-    // Pop our form request from the stack as we're done with it.
-    \Drupal::requestStack()->pop();
-
-    if (empty($entity)) {
-      throw new NotFoundHttpException("Could not find the edited entity");
-    }
-
-    $entity->in_preview = TRUE;
-    $preview_data = \Drupal::service('yoast_seo.entity_previewer')->createEntityPreview($entity);
+    $preview_data = $entity_previewer->createEntityPreview($entity);
 
     // The current value of the alias field, if any,
     // takes precedence over the entity url.
@@ -145,56 +69,4 @@ class EntityPreviewController extends ControllerBase {
     return new JsonResponse($preview_data);
   }
 
-  /**
-   * Create an up to date entity from submitted form data for any entity type.
-   *
-   * Uses a temporary entity and a desired operation to retrieve form values
-   * from the request currently on top of the requestStack and returns an entity
-   * with those updated values.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $temp_entity
-   *   A temporary entity that is used to gather information
-   *   like entity type and bundle.
-   * @param null $operation
-   *   The operation that the submitted form performs on the entity. Required
-   *   to select the correct form display mode and map submitted fields to those
-   *   available in the form.
-   *
-   * @return \Drupal\Core\Entity\Entity
-   *   An entity that contains the values from the submitted form.
-   */
-  protected function getUpdatedEntity($temp_entity, $operation = NULL) {
-    $form_handlers = $temp_entity->getEntityType()->get('handlers')['form'];
-
-    if (empty($operation) || !isset($form_handlers[$operation])) {
-      $operation = 'default';
-    }
-
-    $form_state = new FormState();
-
-    $form_object = \Drupal::entityTypeManager()->getFormObject($temp_entity->getEntityTypeId(), $operation);
-    $form_object->setEntity($temp_entity);
-
-    /** @var \Drupal\Core\Form\FormBuilder $form_builder */
-    $form_builder = \Drupal::service('form_builder');
-
-    $form = $form_builder->buildForm($form_object, $form_state);
-
-    if (!empty($form_state->getErrors())) {
-      // Handle errors!
-      sleep(1);
-    }
-
-    // Build our entity from the form state.
-    /** @var \Drupal\Core\Entity\Entity $entity */
-    $entity = $form_object->buildEntity($form, $form_state);
-
-    // Support ownable entities that might not yet have an owner.
-    if ($entity instanceof EntityOwnerInterface && empty($entity->getOwner())) {
-      $owner = User::load($this->currentUser()->id());
-      $entity->setOwner($owner);
-    }
-
-    return $entity;
-  }
 }
--- src/EntityPreviewer.php
+++ src/EntityPreviewer.php
@@ -2,10 +2,18 @@
 
 namespace Drupal\yoast_seo;
 
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormState;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\metatag\MetatagManagerInterface;
+use Drupal\user\Entity\User;
+use Drupal\user\EntityOwnerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Provides a preview renderer for entities.
@@ -24,7 +32,72 @@ class EntityPreviewer {
     $this->metatagManager = $metatag_manager;
   }
 
+  public function entityFromFormSubmission($action, $method, $form_data) {
+    /** @var \Symfony\Cmf\Component\Routing\ChainRouter $router */
+    $router = \Drupal::service('router.no_access_checks');
+
+    $form_request = Request::create($action, $method, $form_data);
+
+    // Push our form request on the stack so it's used by the form builder.
+    \Drupal::requestStack()->push($form_request);
+
+    try {
+      $match = $router->matchRequest($form_request);
+    }
+    catch (\Exception $e) {
+      throw new NotFoundHttpException('Could not find the entity route.');
+    }
+
+    $operation = NULL;
+
+    // Find the first loaded entity.
+    foreach ($match as $val) {
+      // Existing entities will provide is with an entity of ContentEntityBase.
+      if ($val instanceof ContentEntityBase || is_subclass_of($val, "ContentEntityBase")) {
+        $temp_entity = $val;
+
+        $operation = explode('.', $match['_entity_form'])[1];
+        break;
+      }
+      // Entity creation pages provide us a parent ConfigEntityBase inheritor to
+      // help us determine the entity type and bundle.
+      elseif ($val instanceof ConfigEntityBase || is_subclass_of($val, "ConfigEntityBase")) {
+        $config_entity_type = $val->getEntityType();
+        $type = $config_entity_type->getBundleOf();
+
+        $operation = explode('.', $match['_route'])[1];
+
+        $storage = $this->entityTypeManager()->getStorage($type);
+
+        // Set the bundle name where needed.
+        $type_key = $storage->getEntityType()->get('entity_keys')['bundle'];
+
+        // Create a temporary entity so we can load the form.
+        $temp_entity = $storage->create([$type_key => $val->id()]);
+
+        break;
+      }
+    }
+
+    if (empty($temp_entity)) {
+      throw new BadRequestHttpException('Could not construct entity from form data');
+    }
+
+    $entity = $this->getUpdatedEntity($temp_entity, $operation);
+
+    // Pop our form request from the stack as we're done with it.
+    \Drupal::requestStack()->pop();
+
+    if (empty($entity)) {
+      throw new NotFoundHttpException("Could not find the edited entity");
+    }
+
+    return $entity;
+  }
+
   public function createEntityPreview(EntityInterface $entity) {
+    $entity->in_preview = TRUE;
+
     $html = $this->renderEntity($entity);
 
     $tags = $this->metatagManager->tagsFromEntityWithDefaults($entity);
@@ -39,7 +112,7 @@ class EntityPreviewer {
     foreach ($this->getFieldMappings() as $source => $target) {
       if (isset($data[$source])) {
         $data[$target] = $data[$source];
-//        unset($data[$source]);
+        unset($data[$source]);
       }
     }
 
@@ -67,6 +140,59 @@ class EntityPreviewer {
   }
 
   /**
+   * Create an up to date entity from submitted form data for any entity type.
+   *
+   * Uses a temporary entity and a desired operation to retrieve form values
+   * from the request currently on top of the requestStack and returns an entity
+   * with those updated values.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $temp_entity
+   *   A temporary entity that is used to gather information
+   *   like entity type and bundle.
+   * @param null $operation
+   *   The operation that the submitted form performs on the entity. Required
+   *   to select the correct form display mode and map submitted fields to those
+   *   available in the form.
+   *
+   * @return \Drupal\Core\Entity\Entity
+   *   An entity that contains the values from the submitted form.
+   */
+  protected function getUpdatedEntity($temp_entity, $operation = NULL) {
+    $form_handlers = $temp_entity->getEntityType()->get('handlers')['form'];
+
+    if (empty($operation) || !isset($form_handlers[$operation])) {
+      $operation = 'default';
+    }
+
+    $form_state = new FormState();
+
+    $form_object = \Drupal::entityTypeManager()->getFormObject($temp_entity->getEntityTypeId(), $operation);
+    $form_object->setEntity($temp_entity);
+
+    /** @var \Drupal\Core\Form\FormBuilder $form_builder */
+    $form_builder = \Drupal::service('form_builder');
+
+    $form = $form_builder->buildForm($form_object, $form_state);
+
+    if (!empty($form_state->getErrors())) {
+      // TODO: Handle errors!
+      sleep(1);
+    }
+
+    // Build our entity from the form state.
+    /** @var \Drupal\Core\Entity\Entity $entity */
+    $entity = $form_object->buildEntity($form, $form_state);
+
+    // Support ownable entities that might not yet have an owner.
+    if ($entity instanceof EntityOwnerInterface && empty($entity->getOwner())) {
+      $owner = User::load($this->currentUser()->id());
+      $entity->setOwner($owner);
+    }
+
+    return $entity;
+  }
+
+  /**
    * Returns an array of mappings where metatag field names differ from Yoast
    * expected names.
    *
@@ -78,5 +204,4 @@ class EntityPreviewer {
       'description' => 'meta',
     ];
   }
-
 }
